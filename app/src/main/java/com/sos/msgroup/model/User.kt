b0c26389cb53d89b2a7bad20e35508d887caf144
Thamsanqa Shabalala <thammy202@gmail.com>
2023-04-18 16:40:44 +0200

    Initial commit

---
+++ app/src/main/java/com/sos/msgroup/model/User.kt
@@ -0,0 +1,6 @@
+package com.sos.msgroup.model
+
+data class User(
+    var type: String, val ID_Number: String, var firstName: String, var lastName: String,
+    var gender: String, var phoneNumber: String, var email: String,
+)
