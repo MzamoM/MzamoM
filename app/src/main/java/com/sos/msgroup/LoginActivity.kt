8f935e2d53a5ac6ecfaeaade5bffa57ffcdbf9ed
Thamsanqa Shabalala <thammy202@gmail.com>
2023-08-23 17:57:38 +0200

    Password recovery

--- app/src/main/java/com/sos/msgroup/LoginActivity.kt
+++ app/src/main/java/com/sos/msgroup/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.sos.msgroup
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
@@ -10,18 +11,24 @@ import com.sos.msgroup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    //Forgot password screen
+    //Forgot password screen [Done]
     //Show/hide password [Done]
-    //Admin page
+    //Admin page [Done]
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setMessage("Loading...")
+        progressDialog.setCancelable(false)
 
         auth = FirebaseAuth.getInstance()
     }
@@ -35,16 +42,22 @@ class LoginActivity : AppCompatActivity() {
             binding.editTextLoginPassword.error = "Password required"
             return
         } else{
+
+            progressDialog.show()
+
+
             val email = binding.editTextLoginEmailAddress.text.toString()
             val password = binding.editTextLoginPassword.text.toString()
 
             auth.signInWithEmailAndPassword(email.trim(), password.trim()).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    progressDialog.dismiss()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
                 }
             }.addOnFailureListener { exception ->
+                progressDialog.dismiss()
                 Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
             }
         }
@@ -58,7 +71,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun goToForgotPassword(view: View) {
-        val intent = Intent(this, RegisterActivity::class.java)
+        val intent = Intent(this, ResetPasswordActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/sos/msgroup/ResetPasswordActivity.java
@@ -0,0 +1,75 @@
+package com.sos.msgroup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private Button btnResetPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edtEmail = findViewById(R.id.edt_reset_email);
+        btnResetPassword = findViewById(R.id.btn_reset_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetPasswordActivity.this, "Check email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this, "Fail to send reset password email!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                   }); }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this,LoginActivity.class));
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
